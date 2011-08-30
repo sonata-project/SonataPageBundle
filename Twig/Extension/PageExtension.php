fa842dcfbaee3b3f008bddaffd57cb07f8b156d9
Charley Maillot <cmaillot@ekino.com>
2011-08-30 11:48:08 +0200

    add twig breadcrumb helper

--- Twig/Extension/PageExtension.php
+++ Twig/Extension/PageExtension.php
@@ -13,14 +13,33 @@ namespace Sonata\PageBundle\Twig\Extension;
 
 use Symfony\Component\Routing\Router;
 use Sonata\PageBundle\Model\PageInterface;
+use Sonata\PageBundle\CmsManager\CmsManagerSelectorInterface;
 
 class PageExtension extends \Twig_Extension
 {
+    /**
+     * @var Router
+     */
     private $router;
 
-    public function __construct(Router $router)
+    /**
+     * @var CmsManagerSelectorInterface
+     */
+    private $cmsManagerSelector;
+
+    /**
+     * @var
+     */
+    private $resources;
+
+    /**
+     * @param Router $router
+     * @param CmsManagerSelectorInterface $cmsManagerSelector
+     */
+    public function __construct(Router $router, CmsManagerSelectorInterface $cmsManagerSelector)
     {
         $this->router = $router;
+        $this->cmsManagerSelector = $cmsManagerSelector;
     }
 
     /**
@@ -31,7 +50,8 @@ class PageExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'page_url'  => new \Twig_Function_Method($this, 'url'),
+            'page_url'        => new \Twig_Function_Method($this, 'url'),
+            'page_breadcrumb' =>  new \Twig_Function_Method($this, 'breadcrumb', array('is_safe' => array('html'))),
         );
     }
 
@@ -54,8 +74,32 @@ class PageExtension extends \Twig_Extension
     }
 
     /**
-     * @param string $page
-     * @param boolean $absolute
+     * @param null|\Sonata\PageBundle\Model\PageInterface $page
+     * @param array $options
+     * @return
+     */
+    public function breadcrumb(PageInterface $page = null, array $options = array())
+    {
+        if (!$page) {
+            $page = $this->cmsManagerSelector->retrieve()->getCurrentPage();
+        }
+
+        $defaultOptions = array(
+            'separator'     => '',
+            'current_class' => '',
+            'last_separator'=> ''
+        );
+
+        return $this->render('SonataPageBundle:Page:breadcrumb.html.twig', array(
+            'page'        => $page,
+            'options'     => array_merge($options, $defaultOptions)
+        ));
+    }
+
+    /**
+     * @throws \RunTimeException
+     * @param null|\Sonata\PageBundle\Model\PageInterface|string $page
+     * @param bool $absolute
      * @return string
      */
     public function url($page = null, $absolute = false)
@@ -96,6 +140,20 @@ class PageExtension extends \Twig_Extension
         }
 
         return $url;
+    }
+
+    /**
+     * @param string $template
+     * @param array $parameters
+     * @return string
+     */
+    public function render($template, array $parameters = array())
+    {
+        if (!isset($this->resources[$template])) {
+            $this->resources[$template] = $this->environment->loadTemplate($template);
+        }
+
+        return $this->resources[$template]->render($parameters);
     }
 }
 
