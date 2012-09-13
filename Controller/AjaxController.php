0de39200c326caff08374902c0edde79f0fbb2f4
Vincent Composieux <composieux@ekino.com>
2012-09-13 16:38:42 +0200

    Add block ajax route rendering

---
+++ Controller/AjaxController.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Sonata\PageBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+
+use Sonata\BlockBundle\Exception\BlockNotFoundException;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockRendererInterface;
+
+use Sonata\PageBundle\CmsManager\CmsManagerInterface;
+use Sonata\PageBundle\Exception\PageNotFoundException;
+use Sonata\PageBundle\CmsManager\CmsManagerSelectorInterface;
+use Sonata\PageBundle\Model\PageInterface;
+
+/**
+ * Render a block in ajax
+ *
+ * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
+class AjaxController
+{
+    /**
+     * @var \Sonata\PageBundle\CmsManager\CmsManagerSelectorInterface
+     */
+    protected $cmsManagerSelector;
+
+    /**
+     * @var \Sonata\BlockBundle\Block\BlockRendererInterface
+     */
+    protected $blockRenderer;
+
+    /**
+     * @param \Sonata\PageBundle\CmsManager\CmsManagerSelectorInterface $cmsManagerSelector CMS Manager selector
+     * @param \Sonata\BlockBundle\Block\BlockRendererInterface          $blockRenderer      Block renderer
+     */
+    public function __construct(CmsManagerSelectorInterface $cmsManagerSelector, BlockRendererInterface $blockRenderer)
+    {
+        $this->cmsManagerSelector = $cmsManagerSelector;
+        $this->blockRenderer      = $blockRenderer;
+    }
+
+    /**
+     * Action for ajax route rendering a block by calling his executeAjax() method
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request Symfony request object
+     * @param integer                                   $pageId  Page identifier
+     * @param integer                                   $blockId Block identifier
+     */
+    public function execute(Request $request, $pageId, $blockId)
+    {
+        $cmsManager = $this->cmsManagerSelector->retrieve();
+
+        $page = $cmsManager->getPageById($pageId);
+
+        $renderingBlock = $this->findBlockById($page->getBlocks(), $blockId);
+
+        if (null === $renderingBlock) {
+            throw new BlockNotFoundException(sprintf('Unable to find block identifier "%s" in page "%s".', $blockId, $pageId));
+        }
+
+        $response = $this->blockRenderer->render($renderingBlock);
+
+        return $response;
+    }
+
+    /**
+     * Returns found block in page blocks array
+     *
+     * @param array   $page    Page model
+     * @param integer $blockId Block identifier
+     *
+     * @return BlockInterface|null
+     */
+    protected function findBlockById($blocks, $blockId)
+    {
+        $return = null;
+
+        foreach ($blocks as $block) {
+            if ($block->getId() == (int) $blockId) {
+                $return = $block;
+            } else {
+                $return = $this->findBlockById($block->getChildren(), $blockId);
+            }
+
+            if ($return) {
+                return $return;
+            }
+        }
+
+        return $return;
+    }
+}
