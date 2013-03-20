99d1271d29e8b38421499a395bc85631dbd604eb
Thomas <thomas.rabaix@gmail.com>
2013-03-20 19:11:19 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -103,7 +103,7 @@ class Configuration implements ConfigurationInterface
                     ->arrayNode('esi')
                         ->children()
                             ->scalarNode('token')->defaultValue(hash('sha256', uniqid(mt_rand(), true)))->end()
-                            ->scalarNode('version')->defaultValue(3)->end()
+                            ->scalarNode('version')->defaultValue(2)->end()
                             ->arrayNode('servers')
                                 ->prototype('scalar')->end()
                             ->end()
