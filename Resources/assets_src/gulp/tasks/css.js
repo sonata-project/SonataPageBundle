15ee9af844e1620d0828dbfde1ca17eacd476fbd
Raphaël Benitte <benitteraphael@gmail.com>
2014-11-04 11:01:49 +0100

    Split frontend asset bundles for frontend and backend

--- Resources/assets_src/gulp/tasks/css.js
+++ Resources/assets_src/gulp/tasks/css.js
@@ -5,15 +5,22 @@ var rename = require('gulp-rename');
 var banner = require('../util/banner');
 var concat = require('gulp-concat');
 var config = require('../config');
+var _      = require('lodash');
 
-gulp.task('css', function () {
-    gulp.src(config.source + '/scss/*.scss')
-        .pipe(sass())
-        .pipe(concat('sonata-page.css'))
-        .pipe(banner())
-        .pipe(gulp.dest(config.dest))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(banner())
-        .pipe(gulp.dest(config.dest));
-});
+gulp.task('css', _.map(config.groups.css, function (files, name) {
+    var taskName = 'css:' + name;
+    gulp.task(taskName, function () {
+        gulp.src(_.map(files, function (file) {
+                return config.source + '/scss/' + file;
+            }))
+            .pipe(sass())
+            .pipe(concat('sonata-page.' + name + '.css'))
+            .pipe(banner())
+            .pipe(gulp.dest(config.dest))
+            .pipe(cssmin())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(banner())
+            .pipe(gulp.dest(config.dest));
+    });
+    return taskName;
+}));
--- Resources/assets_src/gulp/tasks/js.js
+++ Resources/assets_src/gulp/tasks/js.js
@@ -4,14 +4,22 @@ var uglify = require('gulp-uglify');
 var banner = require('../util/banner');
 var rename = require('gulp-rename');
 var config = require('../config');
+var _      = require('lodash');
 
-gulp.task('js', function () {
-    gulp.src(config.source + '/js/*.js')
-        .pipe(concat('sonata-page.js'))
-        .pipe(banner())
-        .pipe(gulp.dest(config.dest))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(banner())
-        .pipe(gulp.dest(config.dest));
-});
+gulp.task('js', _.map(config.groups.js, function (files, name) {
+    var taskName = 'js:' + name;
+    var deps     = [];
+    gulp.task(taskName, deps, function () {
+        gulp.src(_.map(files, function (file) {
+                return config.source + '/' + file;
+            }))
+            .pipe(concat('sonata-page.' + name + '.js'))
+            .pipe(banner())
+            .pipe(gulp.dest(config.dest))
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(banner())
+            .pipe(gulp.dest(config.dest));
+    });
+    return taskName;
+}));
