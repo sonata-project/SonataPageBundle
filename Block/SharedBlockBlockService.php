45609416a40d85a25fac4f3ed9baf88bc9f1b027
Romain Mouillard <romain.mouillard@gmail.com>
2014-12-22 16:57:18 +0100

    Fix empty block validation

--- Block/SharedBlockBlockService.php
+++ Block/SharedBlockBlockService.php
@@ -148,7 +148,8 @@ class SharedBlockBlockService extends BaseBlockService
                 'sonata_field_description' => $fieldDescription,
                 'class'                    => $this->getSharedBlockAdmin()->getClass(),
                 'model_manager'            => $this->getSharedBlockAdmin()->getModelManager(),
-                'label'                    => 'block'
+                'label'                    => 'block',
+                'required'                 => false
             ));
     }
 
