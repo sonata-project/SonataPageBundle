adb65a743db3257cb7b607d137137e5c2b872adc
Vincent Composieux <composieux@ekino.com>
2015-02-16 09:22:08 -0500

    Fix SnapshotManager::enableSnapshots() method to end all previous page snapshots

---
+++ Tests/Entity/Snapshot.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PageBundle\Tests\Entity;
+
+use Sonata\PageBundle\Entity\BaseSnapshot;
+
+class Snapshot extends BaseSnapshot
+{
+    /**
+     * @var integer $id
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
--- Tests/Entity/SnapshotManagerTest.php
+++ Tests/Entity/SnapshotManagerTest.php
@@ -127,4 +127,80 @@ class SnapshotManagerTest extends \PHPUnit_Framework_TestCase
             })
             ->getPager(array('site' => 13), 1);
     }
+
+    /**
+     * Tests the enableSnapshots() method to ensure execute queries are correct
+     */
+    public function testEnableSnapshots()
+    {
+        // Given
+        $page = $this->getMock('Sonata\PageBundle\Model\PageInterface');
+        $page->expects($this->once())->method('getId')->will($this->returnValue(456));
+
+        $snapshot = $this->getMock('Sonata\PageBundle\Tests\Entity\Snapshot');
+        $snapshot->expects($this->once())->method('getId')->will($this->returnValue(123));
+        $snapshot->expects($this->once())->method('getPage')->will($this->returnValue($page));
+
+        $connection = $this->getMockBuilder('Doctrine\DBAL\Connection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $date = new \DateTime();
+
+        $connection
+            ->expects($this->once())
+            ->method('query')
+            ->with(sprintf("UPDATE page_snapshot SET publication_date_end = '%s' WHERE id NOT IN(123) AND page_id IN (456)", $date->format('Y-m-d H:i:s')));
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $em->expects($this->once())->method('persist')->with($snapshot);
+        $em->expects($this->once())->method('flush');
+        $em->expects($this->once())->method('getConnection')->will($this->returnValue($connection));
+
+        $manager  = $this->getMockBuilder('Sonata\PageBundle\Entity\SnapshotManager')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getEntityManager', 'getTableName'))
+            ->getMock();
+
+        $manager->expects($this->exactly(3))->method('getEntityManager')->will($this->returnValue($em));
+        $manager->expects($this->once())->method('getTableName')->will($this->returnValue('page_snapshot'));
+
+        // When calling method, expects calls
+        $manager->enableSnapshots(array($snapshot), $date);
+    }
+
+    /**
+     * Tests the enableSnapshots() method to ensure execute queries are not executed when no snapshots are given
+     */
+    public function testEnableSnapshotsWhenNoSnapshots()
+    {
+        // Given
+        $connection = $this->getMockBuilder('Doctrine\DBAL\Connection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $connection->expects($this->never())->method('query');
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $em->expects($this->never())->method('persist');
+        $em->expects($this->never())->method('flush');
+        $em->expects($this->never())->method('getConnection');
+
+        $manager  = $this->getMockBuilder('Sonata\PageBundle\Entity\SnapshotManager')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getEntityManager', 'getTableName'))
+            ->getMock();
+
+        $manager->expects($this->never())->method('getEntityManager');
+        $manager->expects($this->never())->method('getTableName');
+
+        // When calling method, do not expects any calls
+        $manager->enableSnapshots(array());
+    }
 }
