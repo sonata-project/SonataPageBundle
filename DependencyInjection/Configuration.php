24f273169b173fd31568f23bc7d818bf07073554
Oskar Stark <oskarstark@googlemail.com>
2015-10-12 11:14:01 +0200

    added infos to configuration values

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -34,9 +34,42 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $node = $treeBuilder->root('sonata_page')->children();
 
+        $ignoreRoutePatternsInfo = <<<EOF
+(.*)admin(.*)       ignore admin route, i.e. route containing 'admin'
+^_(.*)              ignore Symfony routes
+EOF;
+
+        $ignoreUriPatternsInfo = <<<EOF
+admin(.*)           ignore admin route, i.e. route containing 'admin'
+EOF;
+
+        $pageDefaultsInfo = <<<EOF
+Example:
+homepage: { decorate: false }       disable decoration for 'homepage', key is a page route
+EOF;
+
+        $catchExceptionsInfo = <<<EOF
+Manage the HTTP errors
+
+Examples:
+not_found: [404]    render 404 page with "not_found" key (name generated: _page_internal_error_not_found)
+fatal:     [500]    render 500 page with "fatal" key (name generated: _page_internal_error_fatal)
+EOF;
+
+        $directPublicationInfo = <<<EOF
+Generates a snapshot when a page is saved from the admin.
+
+You can use %kernel.debug%, if you want to publish in dev mode, but not in prod.
+EOF;
+
         $node
-            ->scalarNode('is_inline_edition_on')->defaultFalse()->end()
-            ->scalarNode('use_streamed_response')->defaultFalse()->end()
+            ->scalarNode('is_inline_edition_on')
+                ->defaultFalse()
+            ->end()
+            ->scalarNode('use_streamed_response')
+                ->info('Set the value to false in debug mode or if the reverse proxy does not handle streamed response')
+                ->defaultFalse()
+            ->end()
             ->scalarNode('multisite')
                 ->info('For more information, see https://sonata-project.org/bundles/page/master/doc/reference/multisite.html')
                 ->isRequired()
@@ -45,16 +78,16 @@ class Configuration implements ConfigurationInterface
                     ->thenInvalid('Invalid multisite configuration %s. For more information, see https://sonata-project.org/bundles/page/master/doc/reference/multisite.html')
                 ->end()
             ->end()
-
             ->arrayNode('ignore_route_patterns')
+                ->info($ignoreRoutePatternsInfo)
                 ->defaultValue(array(
-                    '/(.*)admin(.*)/',   # ignore admin route, i.e. route containing 'admin'
-                    '/^_(.*)/',          # ignore symfony routes
+                    '/(.*)admin(.*)/',
+                    '/^_(.*)/',
                 ))
                 ->prototype('scalar')->end()
             ->end()
             ->scalarNode('slugify_service')
-                ->info('you should use: sonata.core.slugify.cocur')
+                ->info('You should use: sonata.core.slugify.cocur, but for BC we keep \'sonata.core.slugify.native\' as default')
                 ->defaultValue('sonata.core.slugify.native')
             ->end()
             ->arrayNode('ignore_routes')
@@ -72,8 +105,9 @@ class Configuration implements ConfigurationInterface
             ->end()
 
             ->arrayNode('ignore_uri_patterns')
+                ->info($ignoreUriPatternsInfo)
                 ->defaultValue(array(
-                    '/admin(.*)/',   # ignore admin route, i.e. route containing 'admin'
+                    '/admin(.*)/',
                 ))
                 ->prototype('scalar')->end()
             ->end()
@@ -90,8 +124,13 @@ class Configuration implements ConfigurationInterface
                 ->end()
             ->end()
 
-            ->scalarNode('default_page_service')->defaultValue('sonata.page.service.default')->end()
-            ->scalarNode('default_template')->isRequired()->end()
+            ->scalarNode('default_page_service')
+                ->defaultValue('sonata.page.service.default')
+            ->end()
+            ->scalarNode('default_template')
+                ->info('Template key from templates section, used as default for pages')
+                ->isRequired()
+            ->end()
 
             ->arrayNode('assets')
                 ->addDefaultsIfNotSet()
@@ -142,7 +181,9 @@ class Configuration implements ConfigurationInterface
                         ->end()
                         ->arrayNode('matrix')
                             ->children()
-                                ->scalarNode('layout')->isRequired()->end()
+                                ->scalarNode('layout')
+                                    ->isRequired()
+                                ->end()
                                 ->arrayNode('mapping')
                                     ->isRequired()
                                     ->requiresAtLeastOneElement()
@@ -187,11 +228,16 @@ class Configuration implements ConfigurationInterface
             ->end()
 
             ->arrayNode('page_defaults')
+                ->info($pageDefaultsInfo)
                 ->useAttributeAsKey('id')
                 ->prototype('array')
                     ->children()
-                        ->booleanNode('decorate')->defaultValue(true)->end()
-                        ->booleanNode('enabled')->defaultValue(true)->end()
+                        ->booleanNode('decorate')
+                            ->defaultValue(true)
+                    ->end()
+                        ->booleanNode('enabled')
+                            ->defaultValue(true)
+                        ->end()
                     ->end()
                 ->end()
             ->end()
@@ -204,7 +250,9 @@ class Configuration implements ConfigurationInterface
                                 ->info('The default value is auto generated every time')
                                 ->defaultValue(hash('sha256', uniqid(mt_rand(), true)))
                             ->end()
-                            ->scalarNode('version')->defaultValue(2)->end()
+                            ->scalarNode('version')
+                                ->defaultValue(2)
+                            ->end()
                             ->arrayNode('servers')
                                 ->prototype('scalar')->end()
                             ->end()
@@ -222,6 +270,7 @@ class Configuration implements ConfigurationInterface
             ->end()
 
             ->arrayNode('catch_exceptions')
+                ->info($catchExceptionsInfo)
                 ->useAttributeAsKey('id')
                 ->prototype('variable')->isRequired()->end()
             ->end()
@@ -229,13 +278,22 @@ class Configuration implements ConfigurationInterface
             ->arrayNode('class')
                 ->addDefaultsIfNotSet()
                 ->children()
-                    ->scalarNode('page')->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Page')->end()
-                    ->scalarNode('snapshot')->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Snapshot')->end()
-                    ->scalarNode('block')->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Block')->end()
-                    ->scalarNode('site')->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Site')->end()
+                    ->scalarNode('page')
+                        ->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Page')
+                    ->end()
+                    ->scalarNode('snapshot')
+                        ->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Snapshot')
+                    ->end()
+                    ->scalarNode('block')
+                        ->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Block')
+                    ->end()
+                    ->scalarNode('site')
+                        ->defaultValue('Application\\Sonata\\PageBundle\\Entity\\Site')
+                    ->end()
                 ->end()
             ->end()
             ->booleanNode('direct_publication')
+                ->info($directPublicationInfo)
                 ->defaultValue(false)
             ->end()
         ;
