f6f998f7d59302bd21a2238b2426b7b03f698337
Artur Vesker <arturvesker@gmail.com>
2017-11-04 23:03:37 +0100

    Mark page as edited on block removing

--- Tests/Admin/BaseBlockAdminTest.php
+++ Tests/Admin/BaseBlockAdminTest.php
@@ -12,6 +12,11 @@
 namespace Sonata\PageBundle\Tests\Admin;
 
 use PHPUnit\Framework\TestCase;
+use Sonata\BlockBundle\Block\BlockServiceManagerInterface;
+use Sonata\BlockBundle\Block\Service\AbstractAdminBlockService;
+use Sonata\PageBundle\Admin\BaseBlockAdmin;
+use Sonata\PageBundle\Model\PageBlockInterface;
+use Sonata\PageBundle\Model\PageInterface;
 
 class BaseBlockAdminTest extends TestCase
 {
@@ -31,5 +36,26 @@ class BaseBlockAdminTest extends TestCase
         $query = $this->createMock('Sonata\AdminBundle\Datagrid\ProxyQueryInterface');
         $idx = [];
         $blockAdmin->preBatchAction('delete', $query, $idx, true);
+    }
+
+    public function testSettingAsEditedOnPreRemove()
+    {
+        $page = $this->createMock(PageInterface::class);
+        $page->expects($this->once())->method('setEdited')->with(true);
+
+        $block = $this->createMock(PageBlockInterface::class);
+        $block->expects($this->once())->method('getPage')->will($this->returnValue($page));
+
+        $blockService = $this->createMock(AbstractAdminBlockService::class);
+        $blockService->expects($this->any())->method('preRemove')->with($block);
+
+        $blockServiceManager = $this->createMock(BlockServiceManagerInterface::class);
+        $blockServiceManager->expects($this->once())->method('get')->with($block)->will($this->returnValue($blockService));
+
+        $blockAdmin = $this->getMockBuilder(BaseBlockAdmin::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $blockAdmin->setBlockManager($blockServiceManager);
+        $blockAdmin->preRemove($block);
     }
 }
