ea81c8f5a6571415f3de30e1182d5763726cd841
Julio Montoya <gugli100@gmail.com>
2017-12-27 15:32:09 +0100

    Update PageAdminController.php

--- src/Controller/PageAdminController.php
+++ src/Controller/PageAdminController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Form\FormView;
 
 /**
  * Page Admin Controller.
