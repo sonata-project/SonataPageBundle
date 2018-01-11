548a98db81cfbaab2803f65c4462617e1af342bd
Ruben Jacobs <ruben.jacobs@intracto.com>
2018-01-11 09:10:48 +0100

    Use class names for form field types

--- src/Block/ChildrenPagesBlockService.php
+++ src/Block/ChildrenPagesBlockService.php
@@ -15,9 +15,13 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\AbstractAdminBlockService;
 use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\CoreBundle\Form\Type\ImmutableArrayType;
 use Sonata\PageBundle\CmsManager\CmsManagerSelectorInterface;
 use Sonata\PageBundle\Exception\PageNotFoundException;
+use Sonata\PageBundle\Form\Type\PageSelectorType;
 use Sonata\PageBundle\Site\SiteSelectorInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Templating\EngineInterface;
@@ -86,24 +90,24 @@ class ChildrenPagesBlockService extends AbstractAdminBlockService
      */
     public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
     {
-        $formMapper->add('settings', 'sonata_type_immutable_array', [
+        $formMapper->add('settings', ImmutableArrayType::class, [
             'keys' => [
-                ['title', 'text', [
+                ['title', TextType::class, [
                   'required' => false,
                     'label' => 'form.label_title',
                 ]],
-                ['current', 'checkbox', [
+                ['current', CheckboxType::class, [
                   'required' => false,
                   'label' => 'form.label_current',
                 ]],
-                ['pageId', 'sonata_page_selector', [
+                ['pageId', PageSelectorType::class, [
                     'model_manager' => $formMapper->getAdmin()->getModelManager(),
                     'class' => $formMapper->getAdmin()->getClass(),
                     'site' => $block->getPage()->getSite(),
                     'required' => false,
                     'label' => 'form.label_page',
                 ]],
-                ['class', 'text', [
+                ['class', TextType::class, [
                   'required' => false,
                   'label' => 'form.label_class',
                 ]],
