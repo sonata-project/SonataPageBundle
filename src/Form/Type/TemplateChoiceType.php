33fe85935d6bd75752cd330703bccdb3bb936006
Grégoire Paris <postmaster@greg0ire.fr>
2020-01-27 20:36:46 +0100

    3.15.0

--- src/Form/Type/TemplateChoiceType.php
+++ src/Form/Type/TemplateChoiceType.php
@@ -49,7 +49,7 @@ class TemplateChoiceType extends AbstractType
     /**
      * NEXT_MAJOR: remove this method.
      *
-     * @deprecated since sonata-project/page-bundle 3.x, to be removed in version 4.0.
+     * @deprecated since sonata-project/page-bundle 3.14, to be removed in version 4.0.
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
@@ -82,7 +82,7 @@ class TemplateChoiceType extends AbstractType
     /**
      * NEXT_MAJOR: remove this method.
      *
-     * @deprecated since sonata-project/page-bundle 3.x, to be removed in version 4.0.
+     * @deprecated since sonata-project/page-bundle 3.14, to be removed in version 4.0.
      */
     public function getName()
     {
