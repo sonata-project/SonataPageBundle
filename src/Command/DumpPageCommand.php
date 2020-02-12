2ef5cbbd9f1211a6e074468706ed7eb8d883051b
Oskar Stark <oskarstark@googlemail.com>
2020-02-12 09:15:18 +0100

    CS fix

--- src/Command/DumpPageCommand.php
+++ src/Command/DumpPageCommand.php
@@ -30,14 +30,16 @@ class DumpPageCommand extends BaseCommand
         $this->setName('sonata:page:dump-page');
         $this->setDescription('Dump page information');
         $this->setHelp(
-'Dump page information
+            <<<HELP
+Dump page information
 
 Available managers:
  - sonata.page.cms.snapshot
  - sonata.page.cms.page
 
 You can use the --extended option to dump block configuration
-');
+HELP
+        );
 
         $this->addArgument('manager', InputArgument::REQUIRED, 'The manager service id');
         $this->addArgument('page_id', InputArgument::REQUIRED, 'The page id');
