d102ce8843f3fb9456ce7d44d3af1194c959e8cb
Christian Gripp <core23@users.noreply.github.com>
2021-12-05 21:55:43 +0100

    Add BlockManagerInterface (#1369)

---
+++ src/Model/BlockManagerInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PageBundle\Model;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\Doctrine\Model\ManagerInterface;
+
+/**
+ * NEXT_MAJOR: Remove Sonata\BlockBundle\Model\BlockManagerInterface inheritance.
+ */
+interface BlockManagerInterface extends ManagerInterface, \Sonata\BlockBundle\Model\BlockManagerInterface
+{
+    /**
+     * Updates position for given block.
+     *
+     * @param int  $id       Block Id
+     * @param int  $position New Position
+     * @param int  $parentId Parent block Id (needed when partial = true)
+     * @param int  $pageId   Page Id (needed when partial = true)
+     * @param bool $partial  Should we use partial references? (Better for performance, but can lead to query issues.)
+     *
+     * @return BlockInterface
+     */
+    public function updatePosition($id, $position, $parentId = null, $pageId = null, $partial = true);
+}
