03c79d66664a7049a263fef06277c57aa01c11a8
Jordi Sala Morales <jordism91@gmail.com>
2022-07-30 19:19:30 +0200

    Remove more deprecated code (#1509)

--- src/Model/Snapshot.php
+++ src/Model/Snapshot.php
@@ -106,11 +106,6 @@ abstract class Snapshot implements SnapshotInterface
     protected $parentId;
 
     /**
-     * @deprecated since sonata-project/page-bundle 2.4 and will be removed in 4.0
-     */
-    protected $sources;
-
-    /**
      * @var SiteInterface|null
      */
     protected $site;
