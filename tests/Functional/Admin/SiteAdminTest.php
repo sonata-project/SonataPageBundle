f3738e6b87fedd2df7931af1d016389a9bc812e6
Jordi Sala Morales <jordism91@gmail.com>
2022-08-10 10:34:45 +0200

    Add tests for Site and Snapshot Admins

---
+++ tests/Functional/Admin/SiteAdminTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PageBundle\Tests\Functional\Admin;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\PageBundle\Tests\App\AppKernel;
+use Sonata\PageBundle\Tests\App\Entity\SonataPageSite;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+final class SiteAdminTest extends WebTestCase
+{
+    /**
+     * @dataProvider provideCrudUrlsCases
+     *
+     * @param array<string, mixed> $parameters
+     */
+    public function testCrudUrls(string $url, array $parameters = []): void
+    {
+        $client = self::createClient();
+
+        $this->prepareData();
+
+        $client->request('GET', $url, $parameters);
+
+        self::assertResponseIsSuccessful();
+    }
+
+    /**
+     * @return iterable<array<string|array<string, mixed>>>
+     *
+     * @phpstan-return iterable<array{0: string, 1?: array<string, mixed>}>
+     */
+    public static function provideCrudUrlsCases(): iterable
+    {
+        yield 'List Site' => ['/admin/tests/app/sonatapagesite/list'];
+        yield 'Create Site' => ['/admin/tests/app/sonatapagesite/create'];
+        yield 'Edit Site' => ['/admin/tests/app/sonatapagesite/1/edit'];
+        yield 'Remove Site' => ['/admin/tests/app/sonatapagesite/1/delete'];
+        yield 'Snaphosts Site' => ['/admin/tests/app/sonatapagesite/1/snapshots'];
+    }
+
+    /**
+     * @return class-string<KernelInterface>
+     */
+    protected static function getKernelClass(): string
+    {
+        return AppKernel::class;
+    }
+
+    /**
+     * @psalm-suppress UndefinedPropertyFetch
+     */
+    private function prepareData(): void
+    {
+        // TODO: Simplify this when dropping support for Symfony 4.
+        // @phpstan-ignore-next-line
+        $container = method_exists($this, 'getContainer') ? self::getContainer() : self::$container;
+        $manager = $container->get('doctrine.orm.entity_manager');
+        \assert($manager instanceof EntityManagerInterface);
+
+        $site = new SonataPageSite();
+        $site->setName('name');
+        $site->setHost('localhost');
+
+        $manager->persist($site);
+
+        $manager->flush();
+    }
+}
