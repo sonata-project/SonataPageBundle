9169c23d1ea6207407c40bba831b6d6baaae7071
Jordi Sala Morales <jordism91@gmail.com>
2022-08-10 12:27:11 +0200

    Add more tests

---
+++ tests/Functional/Admin/BlockAdminTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PageBundle\Tests\Functional\Admin;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\PageBundle\Tests\App\AppKernel;
+use Sonata\PageBundle\Tests\App\Entity\SonataPageBlock;
+use Sonata\PageBundle\Tests\App\Entity\SonataPagePage;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+final class BlockAdminTest extends WebTestCase
+{
+    /**
+     * @dataProvider provideCrudUrlsCases
+     *
+     * @param array<string, mixed> $parameters
+     */
+    public function testCrudUrls(string $url, array $parameters = []): void
+    {
+        $client = self::createClient();
+
+        $this->prepareData();
+
+        $client->request('GET', $url, $parameters);
+
+        self::assertResponseIsSuccessful();
+    }
+
+    /**
+     * @return iterable<array<string|array<string, mixed>>>
+     *
+     * @phpstan-return iterable<array{0: string, 1?: array<string, mixed>}>
+     */
+    public static function provideCrudUrlsCases(): iterable
+    {
+        yield 'List Block' => ['/admin/tests/app/sonatapageblock/list'];
+        yield 'Edit Block' => ['/admin/tests/app/sonatapageblock/1/edit'];
+        yield 'Remove Block' => ['/admin/tests/app/sonatapageblock/1/delete'];
+        yield 'Compose preview Block' => ['/admin/tests/app/sonatapageblock/2/compose_preview'];
+    }
+
+    /**
+     * @return class-string<KernelInterface>
+     */
+    protected static function getKernelClass(): string
+    {
+        return AppKernel::class;
+    }
+
+    /**
+     * @psalm-suppress UndefinedPropertyFetch
+     */
+    private function prepareData(): void
+    {
+        // TODO: Simplify this when dropping support for Symfony 4.
+        // @phpstan-ignore-next-line
+        $container = method_exists($this, 'getContainer') ? self::getContainer() : self::$container;
+        $manager = $container->get('doctrine.orm.entity_manager');
+        \assert($manager instanceof EntityManagerInterface);
+
+        $page = new SonataPagePage();
+        $page->setName('name');
+        $page->setTemplateCode('default');
+
+        $parentBlock = new SonataPageBlock();
+        $parentBlock->setType('sonata.page.block.container');
+        $parentBlock->setPage($page);
+
+        $block = new SonataPageBlock();
+        $block->setType('sonata.block.service.text');
+        $block->setParent($parentBlock);
+
+        $manager->persist($page);
+        $manager->persist($parentBlock);
+        $manager->persist($block);
+
+        $manager->flush();
+    }
+}
