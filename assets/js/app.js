6633a1e8188982b1ec8eb1f0cadb50abe4e0a304
Jordi Sala Morales <jordism91@gmail.com>
2022-08-15 16:54:34 +0200

    Wip even more

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,16 +10,9 @@
 // Any SCSS/CSS you require will output into a single css file (app.css in this case)
 import '../scss/app.scss';
 
-// Require jQuery normally
-import $ from 'jquery';
-
 // Only using droppable widget from jQuery UI library
 import 'jquery-ui/ui/widgets/droppable';
 
 // SonataPage custom scripts
 import './composer';
 import './treeview';
-
-// Create global $ and jQuery variables to be used outside this script
-global.$ = $;
-global.jQuery = $;
