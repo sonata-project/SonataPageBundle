85f0f12d9067406c6680e2af991c657263b3c55c
Jordi Sala Morales <jordism91@gmail.com>
2022-09-28 09:14:08 +0200

    Add browser test for errors

--- tests/Functional/Admin/BrowserTest.php
+++ tests/Functional/Admin/BrowserTest.php
@@ -14,8 +14,11 @@ declare(strict_types=1);
 namespace Sonata\PageBundle\Tests\Functional\Admin;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Facebook\WebDriver\WebDriverElement;
 use Sonata\PageBundle\Tests\App\Entity\SonataPagePage;
+use Sonata\PageBundle\Tests\App\Entity\SonataPageSite;
 use Sonata\PageBundle\Tests\Functional\BasePantherTestCase;
+use Symfony\Component\DomCrawler\Form;
 
 final class BrowserTest extends BasePantherTestCase
 {
@@ -30,19 +33,57 @@ final class BrowserTest extends BasePantherTestCase
 
         $client->waitForElementToContain('.page-composer__container__view__header', 'Top content');
 
-        static::assertCount(0, $crawler->filter('.page-composer__container__children li'));
+        static::assertCount(0, $crawler->filter('.page-composer__container__child'));
 
         $crawler->filter('.page-composer__block-type-selector .select2')->first()->click();
         $crawler->filter('.select2-results__options li')->first()->click();
         $crawler->filter('.page-composer__block-type-selector__confirm')->first()->click();
 
-        $client->waitFor('.page-composer__container__child');
+        $client->waitFor('.page-composer__container__child__content form');
 
-        $crawler->filter('.page-composer__container__child button')->first()->click();
+        $form = $crawler->selectButton('Create')->form();
+        $client->submit($form);
 
         $client->waitFor('.page-composer__container__child__enabled');
 
-        static::assertCount(1, $crawler->filter('.page-composer__container__children li'));
+        static::assertCount(1, $crawler->filter('.page-composer__container__child'));
+    }
+
+    public function testComposePageBlockErrors(): void
+    {
+        $client = self::createPantherClient();
+        $crawler = $client->request('GET', '/admin/tests/app/sonatapagepage/1/compose');
+
+        $topContentBlock = $crawler->selectLink('Top content')->link();
+
+        $client->click($topContentBlock);
+
+        $client->waitForElementToContain('.page-composer__container__view__header', 'Top content');
+
+        $crawler->filter('.page-composer__block-type-selector .select2')->first()->click();
+        $rssFeed = $crawler->filter('.select2-results__options li')->getElement(1);
+        \assert($rssFeed instanceof WebDriverElement);
+
+        $rssFeed->click();
+        $crawler->filter('.page-composer__block-type-selector__confirm')->first()->click();
+
+        $client->waitFor('.page-composer__container__child__content form');
+
+        $form = $crawler->selectButton('Create')->form();
+        $client->submit($form);
+
+        $client->waitFor('.form-group.has-error');
+
+        $uniqid = $this->extractUniqId($form);
+
+        $form[$uniqid.'[settings][url]'] = 'https://docs.sonata-project.org';
+        $form[$uniqid.'[settings][title]'] = 'Custom title';
+
+        $client->submit($form);
+
+        $client->waitFor('.page-composer__container__child__enabled');
+
+        static::assertCount(2, $crawler->filter('.page-composer__container__child'));
     }
 
     /**
@@ -56,13 +97,31 @@ final class BrowserTest extends BasePantherTestCase
         $manager = $container->get('doctrine.orm.entity_manager');
         \assert($manager instanceof EntityManagerInterface);
 
+        $site = new SonataPageSite();
+        $site->setName('name');
+        $site->setHost('localhost');
+
         $page = new SonataPagePage();
         $page->setName('name');
         $page->setUrl('/');
         $page->setTemplateCode('default');
+        $page->setSite($site);
 
+        $manager->persist($site);
         $manager->persist($page);
 
         $manager->flush();
+    }
+
+    private function extractUniqId(Form $form): string
+    {
+        $url = $form->getUri();
+
+        $queryString = parse_url($url, \PHP_URL_QUERY);
+        \assert(\is_string($queryString));
+
+        parse_str($queryString, $queryParameters);
+
+        return $queryParameters['uniqid'] ?? '';
     }
 }
