d8573d92dafee1cfd8a573a4c5aa85e0bcb6eab0
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-06-19 12:21:46 +0200

    Solve internal class usage (#1767)

--- src/DependencyInjection/SonataPageExtension.php
+++ src/DependencyInjection/SonataPageExtension.php
@@ -20,10 +20,10 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
